e36393512f3746000ee03c4e238244294075a1bb
James Carnegie <me@kipz.org>
2018-03-06 09:51:27 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/QconKipzApplication.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QconKipzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QconKipzApplication.class, args);
+	}
+}
